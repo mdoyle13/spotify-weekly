26c6a563924e1a821ac16c6ef98eb6e20177704a
mike doyle <mdoyle13@gmail.com>
2018-02-20 15:35:12 -0600

    add some user unit tests

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -5,8 +5,9 @@ class PlaylistTest < ActiveSupport::TestCase
   test "should not save with non unique user id and week of" do
     user = users(:one)
     playlist = user.playlists.first
+    # try to save a new playlist with a matching week of as the current users first playlist
     new_playlist = user.playlists.new(week_of: playlist.week_of)
-
+    # validation should fail
     assert_not new_playlist.save
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "has_discover_weekly? returns true if the user record has this data" do 
+    user = users(:one)
+    assert user.has_discover_weekly?
   end
+
+  test "has_disocver_weekly? returns false if the user does not have this field" do
+    user = users(:no_weekly)
+    assert_not user.has_discover_weekly?
+  end
+
+  test "has_backups? returns true if the user has any playlist records attached" do 
+    user = users(:one)
+    assert user.has_backups?
+  end
+
+  test "has_backups? returns false if the user does not have any" do
+    user = users(:no_backups)
+    assert_not user.has_backups?
+  end
+
 end
