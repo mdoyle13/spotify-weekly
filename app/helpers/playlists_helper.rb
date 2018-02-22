3a3c0c497bfeb69056d5b040008bfcc8251b0258
mike doyle <mdoyle13@gmail.com>
2018-02-21 20:58:34 -0600

    add helpers for track images and embed

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,12 @@
 module PlaylistsHelper
+  def track_image_tag(track)
+    return false unless track.small_image
+    image_tag track.small_image
+  end
+
+  def track_embed(track)
+    # <iframe src="https://open.spotify.com/embed/track/7702PAEjAWKu3o6e3qgLoc" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+    content_tag :iframe, nil, src: "https://open.spotify.com/embed/track/#{track.spotify_id}",
+      width: "80", height: "80", frameborder: 0, allowtransparency: true
+  end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,8 +1,20 @@
 class Track < ApplicationRecord
-    store_accessor :data, :name, :id, :album, :artists
-    belongs_to :playlist
+  store_accessor :data, :name, :id, :artists
+  belongs_to :playlist
 
-    def formatted_artist_name
-        artists.map {|a| a["name"] }.join(", ")
-    end
+  def formatted_artist_name
+    artists.map {|a| a["name"] }.join(", ")
+  end
+
+  def images
+    data.dig("album", "images").collect { |i| i["url"] } rescue []
+  end
+
+  def small_image
+    images.last
+  end
+
+  def large_image
+    images.first
+  end
 end
