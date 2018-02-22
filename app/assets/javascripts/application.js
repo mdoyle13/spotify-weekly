54fcf79ae6f7165f69420d6c693b6ddfd66a907b
mike doyle <mdoyle13@gmail.com>
2018-02-21 22:24:45 -0600

    try this i guess

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
-//= require_tree .
-
 //= require jquery3
 //= require jquery_ujs
+//= require turbolinks
 //= require popper
 //= require bootstrap-sprockets
