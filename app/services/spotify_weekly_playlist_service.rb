04dd299e9b41eaac8a2c099783424ad1864d5d55
mike doyle <mdoyle13@gmail.com>
2018-03-04 11:03:13 -0600

    fix typo and get ALL the users playlists to find the discover weekly

--- app/services/spotify_weekly_playlist_service.rb
+++ app/services/spotify_weekly_playlist_service.rb
@@ -12,13 +12,27 @@ class SpotifyWeeklyPlaylistService < BaseSpotifyService
       @response.send("success?=", true)
       @response.playlist = playlist
     else
-      @response.message = "You are not following Discover Wekly. Once you do that come back and try again"
+      @response.message = "You are not following Discover Weekly. Once you do that come back and try again"
     end
     @response
   end
 
   private
   def get_weekly_playlist
-    playlist = spotify_user.playlists.select {|p| p.name == "Discover Weekly"}[0]
+    offset = 0
+    playlists = get_all_playlists
+    playlist = playlists.select {|p| p.name == "Discover Weekly"}[0]
+  end
+
+  def get_all_playlists
+    playlists = []
+    offset = 0
+    loop do
+      _playlists = spotify_user.playlists(limit: 50, offset: offset)
+      break if _playlists.count == 0
+      playlists << _playlists
+      offset += 50
+    end
+    playlists.flatten
   end
 end
